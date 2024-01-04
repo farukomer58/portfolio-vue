346b10903402b8dfdfe74a0ec52f869b2f830ccd
Omer <of.c.58@hotmail.com>
2024-01-04 04:26:20 +0100

    NProgress bar

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import App from './App.vue'
 import router from './router'
 import i18n from './plugins/i18n'
 
+import progressBar from './plugins/progress-bar'
+
+progressBar(router);
+
 const app = createApp(App)
 
 app.use(createPinia())
